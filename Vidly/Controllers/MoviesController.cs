37c0e34be05c9ce09a3b98596bc4be5d4bca5aae
Vinicius Smadeski <vinicius.js94@gmail.com>
2018-12-06 23:44:03 -0200

    Complete Section 2 Exercise.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -11,7 +13,16 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Index()
         {
-            return View();
+            var model = new MoviesIndexViewModel()
+            {
+                Movies = new List<Movie>()
+                {
+                    new Movie(){Id = 1, Name = "Shrek"},
+                    new Movie(){Id = 2, Name = "Wall-e"}
+                }
+            };
+
+            return View(model);
         }
     }
 }
